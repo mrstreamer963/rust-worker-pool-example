fa94482bf8eb62f38e38c178447ab2ba82748dd2
mrstreamer963 <698lo7epy@mozmail.com>
2025-10-12 19:45:03 +0300

    fix

--- src/worker_pool.rs
+++ src/worker_pool.rs
@@ -1,5 +1,5 @@
-#[cfg(not(feature = "web"))]
+#[cfg(not(target_arch = "wasm32"))]
 pub use crate::native_worker_pool::NativeWorkerPool as WorkerPool;
 
-#[cfg(feature = "web")]
+#[cfg(target_arch = "wasm32")]
 pub use crate::web_worker_pool::WebWorkerPool as WorkerPool;
