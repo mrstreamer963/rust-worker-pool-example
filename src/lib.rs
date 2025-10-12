443226e2a101612415a72ea70bd2430449d19324
mrstreamer963 <698lo7epy@mozmail.com>
2025-10-12 13:50:51 +0300

    some updates

--- src/lib.rs
+++ src/lib.rs
@@ -41,7 +41,6 @@ mod wasm {
 
     #[wasm_bindgen(start)]
     pub fn main() {
-        // Можно добавить panic hook для отладки
-        // console_error_panic_hook::set_once();
+        console_error_panic_hook::set_once();
     }
 }
